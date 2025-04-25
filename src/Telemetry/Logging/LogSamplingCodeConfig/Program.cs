77bb372f19bf3ca4fe513b284db09dc06cb4d9d8
Evgenii Fedorov <evgenii.fedorov@microsoft.com>
2025-04-25 12:45:44 +0200

    Add more logging sampling samples

---
+++ src/Telemetry/Logging/LogSamplingCodeConfig/Program.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Threading;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.Sampling;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace LogSamplingCodeConfig;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        var hostBuilder = Host.CreateApplicationBuilder();
+        hostBuilder.Logging.AddSimpleConsole(options =>
+        {
+            options.SingleLine = true;
+            options.TimestampFormat = "hh:mm:ss";
+        });
+
+        // Add the Random probabilistic sampler to the logging pipeline.
+        hostBuilder.Logging.AddRandomProbabilisticSampler(options =>
+        {
+            options.Rules.Add(
+                new RandomProbabilisticSamplerFilterRule(
+                    probability: .1d,
+                    categoryName: "System.*"));
+            options.Rules.Add(
+                new RandomProbabilisticSamplerFilterRule(
+                    probability: .25d,
+                    categoryName: "Microsoft.AspNetCore.*",
+                    eventId: 1001));
+            options.Rules.Add(
+                new RandomProbabilisticSamplerFilterRule(
+                    probability: .05d,
+                    eventId : 1001));
+        });
+
+        var app = hostBuilder.Build();
+        var loggerFactory = app.Services.GetRequiredService<ILoggerFactory>();
+        var logger = loggerFactory.CreateLogger("SamplingDemo");
+
+        while (true)
+        {
+            Log.EnteredWhileLoop(logger);
+            Log.NoisyMessage(logger);
+            Log.LeftWhileLoop(logger);
+
+            Thread.Sleep(100);
+        }
+    }
+}
